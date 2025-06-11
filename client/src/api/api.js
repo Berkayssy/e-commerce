8d21b801a085a9aaf6a189c0ea5ea06308a4ae33
Berkay Aksu <berkay@csgold.bet>
2025-06-11 03:17:31 +0300

    Full-Stack services created

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api", // backend port
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 instance.interceptors.request.use(
