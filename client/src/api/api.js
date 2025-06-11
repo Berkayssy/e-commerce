6d2ff31727887782d6d426afdc22a0889363d746
Berkay Aksu <berkay@csgold.bet>
2025-06-11 14:02:25 +0300

    mongo match atlas to compass

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL || "https://e-commerce-backend-ml1p.onrender.com"
+
 });
 
 instance.interceptors.request.use(
